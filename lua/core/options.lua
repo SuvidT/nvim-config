776715102c67fcba140b9b70b04a0f6cb793a9cc
SuvidT <Suvid2004@gmail.com>
2025-10-18 22:35:06 -0400

    options are stable, but probably still want more

---
+++ lua/core/options.lua
@@ -0,0 +1,27 @@
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.mouse = "a"
+o.ignorecase = true
+o.smartcase = true
+o.hlsearch = false
+o.wrap = false
+o.breakindent = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+o.termguicolors = true
+o.signcolumn = "yes"
+o.updatetime = 250
+o.timeoutlen = 500
+o.completeopt = "menu,menuone,noselect"
+o.undofile = true
+o.splitright = true
+o.splitbelow = true
+o.scrolloff = 8
