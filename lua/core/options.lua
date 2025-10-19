5dde134f94d5082864db7fc23203fa7b88c0247f
SuvidT <Suvid2004@gmail.com>
2025-10-19 04:26:23 -0400

    stable after a options.lua change

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,3 +1,5 @@
+
+--[[
 local o = vim.opt
 
 o.number = true
@@ -22,16 +24,78 @@ o.splitright = true
 o.splitbelow = true
 o.scrolloff = 8
 o.clipboard = "unnamedplus"
+]]
 
---[[
 vim.g.autoformat = true
 vim.g.snacks_animate = true
 vim.g.lazyvim_picker = "auto"
 vim.g.lazyvim_cmp = "auto"
 vim.g.ai_cmp = false
-vim.g.root_spec = {"lsp", {".git", "lua"}, "cwd"}
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+-- vim.g.root_lsp_ignore = { "copilot" }
 vim.g.deprecation_warnings = true
 vim.g.trouble_lualine = true
 
-vim.opt.autowrite = true
-]]
+local opt = vim.opt
+
+opt.autowrite = true
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 2
+opt.confirm = true 
+opt.cursorline = true
+opt.expandtab = true
+opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
+opt.foldlevel = 99
+opt.foldmethod = "indent"
+opt.foldtext = ""
+opt.formatexpr = "v:lua.LazyVim.format.formatexpr()"
+opt.formatoptions = "jcroqlnt"
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true
+opt.inccommand = "nosplit"
+opt.jumpoptions = "view"
+opt.laststatus = 3
+opt.linebreak = true
+opt.list = true
+opt.mouse = "a"
+opt.number = true
+opt.pumblend = 10
+opt.pumheight = 10
+opt.relativenumber = true
+opt.ruler = true
+opt.scrolloff = 8
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.shiftround = true
+opt.shiftwidth = 4
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+opt.showmode = false
+opt.sidescrolloff = 8
+opt.signcolumn = "yes"
+opt.smartcase = true
+opt.smartindent = true
+opt.smoothscroll = true
+opt.spelllang = { "en" }
+opt.splitbelow = true
+opt.splitkeep = "screen"
+opt.splitright = true
+opt.tabstop = 4
+opt.termguicolors = true
+opt.timeoutlen = vim.g.vscode and 1000 or 300
+opt.undofile = true
+opt.undolevels = 10000
+opt.updatetime = 200
+opt.virtualedit = "block"
+opt.wildmode = "longest:full,full"
+opt.winminwidth = 5
+opt.wrap = false
+
+vim.g.markdown_recommended_style = 0
