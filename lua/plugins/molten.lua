7e6e59ed6082f225a818d4d944bc71cc1a3078d8
SuvidT <Suvid2004@gmail.com>
2025-10-25 16:36:48 -0400

    test

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -10,7 +10,7 @@ return {
         -- Core Molten Settings
         vim.g.molten_image_provider = 'image.nvim'
         vim.g.molten_output_win_max_height = 20
-        vim.g.molten_auto_open_output = true
+        vim.g.molten_auto_open_output = false
         vim.g.molten_wrap_output = true
         vim.g.molten_virt_text_output = true
         vim.g.molten_virt_lines_off_by_1 = true
